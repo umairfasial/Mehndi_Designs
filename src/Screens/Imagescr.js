24f4f9063b9e859687850f13ef561c42e8656252
umairfasial <93310704+umairfasial@users.noreply.github.com>
2022-09-27 00:59:15 -0700

    Patch

--- src/Screens/Imagescr.js
+++ src/Screens/Imagescr.js
@@ -3,16 +3,28 @@ import React from 'react'
 import Customheader from '../components/Customheader'
 import Theme from '../utils/Themes'
 import FastImage from 'react-native-fast-image'
+import Carousel from 'react-native-snap-carousel'
+import { List } from '../Assests/Constants/Dumydata'
 
 const Imagescr = ({route}) => {
     const { openimage } = route.params;
   return (
     <View style={{flex:1,backgroundColor:Theme.Black}}>
       <Customheader/>
-     
-      <ScrollView style={{flex:1}}>
-     <FastImage  style={{width:'100%',height:'100%',}} resizeMode="contain" source={openimage}/>
-     </ScrollView>
+     <Carousel 
+     loop
+     onSnapToItem={openimage}
+     data={List}
+     autoplay={true}
+     renderItem={({openimage})=>
+    (
+      <View style={{flex:1,justifyContent:'center'}}>
+      <FastImage  style={{width:'100%',height:'100%',}} resizeMode="contain" source={openimage}/>
+      </View>
+    )
+    }
+     />
+      
   
     </View>
   )
